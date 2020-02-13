c5e6af23e25c498c7022429188cc68b17e643d2b
Marco Mannara <Cr4zyRi0s.channel@gmail.com>
2020-02-13 16:32:13 +0100

    added table view controller and table cell

---
+++ Homer/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  Homer
+//
+//  Created by Marco Mannara on 13/02/2020.
+//  Copyright © 2020 Lorenzo Fasolino. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
