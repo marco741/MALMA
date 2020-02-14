348c6b4681911afba9e224b1aa3159714b2c9ea5
Alessandro De Stefano <alexdsgm98@gmail.com>
2020-02-14 15:16:45 +0100

    border to content view in collection cell

---
+++ Homer/CategoryContentView.swift
@@ -0,0 +1,55 @@
+//
+//  CategoryContentView.swift
+//  Homer
+//
+//  Created by Alessandro De Stefano on 14/02/2020.
+//  Copyright © 2020 Lorenzo Fasolino. All rights reserved.
+//
+
+import UIKit
+
+class CategoryContentView: UIView {
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+         // Drawing code
+     }
+     */
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
