4a3d0d620b8bf46528755ba5a15e23c7608d85f9
Marco Mannara <Cr4zyRi0s.channel@gmail.com>
2020-02-14 16:31:26 +0100

    updated achievement collection view

---
+++ Homer/AchievementCell.swift
@@ -0,0 +1,14 @@
+//
+//  AchievementCell.swift
+//  Homer
+//
+//  Created by Marco Mannara on 14/02/2020.
+//  Copyright © 2020 Lorenzo Fasolino. All rights reserved.
+//
+
+import UIKit
+
+class AchievementCell: UICollectionViewCell {
+    
+    @IBOutlet var cellImage: UIImageView!
+}
