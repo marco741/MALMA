fbd81e8b52adb0506bea2e8c3d71ca57dc79301d
Alfredo <alfredog92@gmail.com>
2020-02-17 11:20:13 +0100

    category tasks storyboard

---
+++ Homer/CategoryTasksCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTasksCell.swift
+//  Homer
+//
+//  Created by Alfredo Giuliano on 17/02/2020.
+//  Copyright © 2020 Lorenzo Fasolino. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTasksCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Homer/CategoryTasksViewController.swift
@@ -0,0 +1,50 @@
+//
+//  CategoryTasksViewController.swift
+//  Homer
+//
+//  Created by Alfredo Giuliano on 17/02/2020.
+//  Copyright © 2020 Lorenzo Fasolino. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTasksViewController: UIViewController, UITableViewDelegate,UITableViewDataSource  {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "categoryTasksCell", for: indexPath)
+        return cell
+    }
+    
+
+    
+    @IBOutlet weak var navigationBar: UINavigationItem!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title="titolo"
+
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
