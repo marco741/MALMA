e025107e347c6b68700893e1be43bb02a22d297b
Lorenzo Fasolino <fasolinolorenzo@gmail.com>
2020-02-17 11:21:20 +0100

    category cell

---
+++ Homer/CategoriesCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  Homer
+//
+//  Created by Lorenzo Fasolino on 17/02/2020.
+//  Copyright © 2020 Lorenzo Fasolino. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var taskNumLabel: UILabel!
+    @IBOutlet var categoryImageView: UIImageView!
+}
