fa98a3c4dd36041ddaa1ea14036cd4491986870b
Alfredo <alfredog92@gmail.com>
2020-02-18 10:24:40 +0100

    categorytasks storyboard fix segment control & checkbox switch saved in db

--- Homer/CategoryTasksCell.swift
+++ Homer/CategoryTasksCell.swift
@@ -18,9 +18,11 @@ class CategoryTasksCell: UITableViewCell {
     @IBOutlet weak var enableButton: UIButton!
     
     
+    var task: Task?
     
     override func awakeFromNib() {
         super.awakeFromNib()
+
         weeklyLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 4)
         checkBox.setImage(UIImage(systemName: "circle"), for: .normal)
         checkBox.setImage(UIImage(systemName: "largecircle.fill.circle"), for: .selected)
@@ -36,36 +38,27 @@ class CategoryTasksCell: UITableViewCell {
     
     
     @IBAction func checkboxPressed(_ sender: Any) {
+        if checkBox.isSelected {
+            task?.unCheck()
+        }
+        else {
+            task?.check()
+        }
         checkBox.isSelected=(!checkBox.isSelected)
     }
+    
     
     @IBAction func enablePressed(_ sender: Any) {
         checkBox.isHidden=false
         enableButton.isHidden=true
+        task?.enable()
+
     }
     
     
     
     
 
-    var task: Task? {
-        willSet{
-            if let cellTask = newValue{
-                print("checked\(cellTask.isChecked())")
-               if(cellTask.isChecked()){
-                   checkBox.isSelected = true
-               }else{
-                   checkBox.isSelected = false
-               }
-                weeklyLabel.isHidden=(!cellTask.weekly)
-                print("weekly: \(cellTask.weekly)")
-                
-               ecoPointsLabel.text = String(cellTask.ecoPoints) + "EP"
-               savingsLabel.text = String(cellTask.savings) + "$"
-               descriptionLabel.text = cellTask.desc
-           }
-       }
-        
-    }
+
 
 }
--- Homer/CategoryTasksViewController.swift
+++ Homer/CategoryTasksViewController.swift
@@ -9,53 +9,101 @@
 import UIKit
 
 class CategoryTasksViewController: UIViewController, UITableViewDelegate,UITableViewDataSource  {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return NStasks!.count
+        if segment!.selectedSegmentIndex==0{
+            return allTasks.count
+        }
+        else{
+            return disabledTasks.count
+        }
+        //            return NStasks!.count
     }
     
+    
+
+//    riempimento e riutilizzo celle
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=tableView.dequeueReusableCell(withIdentifier: "categoryTasksCell", for: indexPath) as! CategoryTasksCell
         print("row: \(indexPath.row)")
         let task:Task
-        task=tasks[indexPath.row]
+        if segment!.selectedSegmentIndex==0{
+            task=allTasks[indexPath.row]
+        }
+        else{
+            task=disabledTasks[indexPath.row]
+            
+        }
         cell.descriptionLabel.text=task.desc
         cell.savingsLabel.text="\(task.savings)$"
         cell.ecoPointsLabel.text="\(task.ecoPoints)EP"
         cell.checkBox.isSelected=task.isChecked()
+        cell.checkBox.isHidden=false
         cell.weeklyLabel.isHidden=(!task.weekly)
         cell.enableButton.isHidden=true
         if task.state=="disabled"{
             switchState(cell)
         }
+        cell.task=task
         return cell
     }
     
-
+    
     
     @IBOutlet weak var navigationBar: UINavigationItem!
     @IBOutlet weak var tableView: UITableView!
     
+    
+//    Delete
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        print("edit mode!")
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            let task : Task =  allTasks[indexPath.row]
+            task.disable()
+            tableView.reloadData()
 
+            print("delete!")
+        }
+    }
+    
+    
+//    Delete funzionante solo per task con stato non "disabled"
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if segment!.selectedSegmentIndex==0{
+            let task=allTasks[indexPath.row]
+            if task.state=="disabled" {
+                return false
+            }
+            return true
+        }
+        return false
+        
+    }
+    
     
     var segment: UISegmentedControl?
     var category:String="titolo"
-    var tasks: [Task] = []
+    var allTasks: [Task] = []
     var NStasks:NSSet?
     var disabledTasks: [Task]=[]
-
     
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         let header  = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 70))
-        
         let label = UILabel()
         label.frame = CGRect.init(x: 20, y: 5, width: header.frame.width-10, height: header.frame.height-10)
-        label.text = category
+        let categoryUppercased:String = category.prefix(1).uppercased() + category.dropFirst()
+        label.text = categoryUppercased
         label.font = UIFont.boldSystemFont(ofSize: 30.0)
+        label.textColor = UIColor(red: 12/255, green: 97/255, blue: 97/255, alpha: 1.0)
         header.addSubview(label)
         tableView.tableHeaderView = header
+        
+
+        
         
         
         segment = UISegmentedControl(items: ["All", "Disabled"])
@@ -66,20 +114,15 @@ class CategoryTasksViewController: UIViewController, UITableViewDelegate,UITable
         
         
         self.navigationItem.titleView = segment
+
+        navigationController?.navigationBar.tintColor = UIColor(red: 18/255, green: 148/255, blue: 148/255, alpha: 1.0)
+
+
         tableView.delegate = self
         tableView.dataSource = self
         
-        NStasks=PMCategory.fetchByName(name: category)[0].tasks!
-        tasks = []
-        disabledTasks = []
-
-        for object in NStasks! {
-             tasks.append(object as! Task)
-            if tasks.last?.state=="disabled"{
-                disabledTasks.append(object as! Task)
-            }
-        }
-
+        reloadTasks()
+        
         // Do any additional setup after loading the view.
     }
     
@@ -93,23 +136,45 @@ class CategoryTasksViewController: UIViewController, UITableViewDelegate,UITable
     @IBAction func segmentValueChanged(){
         switch segment!.selectedSegmentIndex{
         case 0: print("0")
+        reloadTasks()
+        tableView.reloadData()
+        
         case 1:print("1")
+        reloadTasks()
+        tableView.reloadData()
         default: print("Error in switch")
         }
         
     }
     
+    private func reloadTasks(){
+        print("ricarico i task")
+        NStasks=PMCategory.fetchByName(name: category)[0].tasks!
+        allTasks = []
+        disabledTasks = []
+        
+        for object in NStasks! {
+            allTasks.append(object as! Task)
+            if allTasks.last?.state=="disabled"{
+                disabledTasks.append(object as! Task)
+            }
+        }
+    }
     
 
+    
+    
+    
+    
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
